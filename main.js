aade4aa1ae86702047e1742c93d9ad11ee4b2db7
Ugwu Abraham Chidozie <jonasbraham@gmail.com>
2020-02-06 23:50:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function reset(){
         }
        
     }
-    h1.style.backgroundColor = "steelblue";
+    h1.style.backgroundColor = "#A3AF4D";
 }
 
 resetButton.addEventListener("click", function(){
